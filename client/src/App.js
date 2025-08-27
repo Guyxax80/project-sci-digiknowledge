969cc736b541d157a8511d6648c17ef5bdea6049
Guyxax80 <guyxax80@gmail.com>
2025-08-28 01:49:04 +0700

    update document

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Upload from "./pages/Upload";
-import DocumentList from "./pages/DocumentList";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/studentDashboard";
 import TeacherDashboard from "./pages/TeacherDashboard"; // หากมีหน้า teacher
 import Home from "./pages/Home";
+import DocumentPage from "./pages/DocumentPage";
 import './output.css'
 
 function App() {
@@ -17,13 +17,13 @@ function App() {
       <Routes>
 
         <Route path="/upload" element={<Upload />} />
-        <Route path="/documents" element={<DocumentList />} />
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/student" element={<StudentDashboard />} />
         <Route path="/teacher" element={<TeacherDashboard />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/document/:id" element={<DocumentPage />} />
       </Routes>
     </Router>
   );
